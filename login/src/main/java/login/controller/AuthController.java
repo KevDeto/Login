d489c3afd5501a6df147d55f18f3dee3d044eb36
KevDeto <contactohuayta@gmail.com>
2025-01-03 20:01:59 -0300

    endpoints de auth

---
+++ login/src/main/java/login/controller/AuthController.java
@@ -0,0 +1,23 @@
+package login.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("auth/v1")
+@RequiredArgsConstructor
+public class AuthController {
+
+	@PostMapping("login")
+	public String login() {
+		return "login completado";
+	}
+	
+	@PostMapping("register")
+	public String register() {
+		return "registro completado";
+	}
+}
