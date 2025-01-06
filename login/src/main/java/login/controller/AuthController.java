36030da571b9762e4cae76b167293489e5b27942
KevDeto <contactohuayta@gmail.com>
2025-01-06 19:12:17 -0300

    modificado para funcionar con las nuevas configuraciones

--- login/src/main/java/login/controller/AuthController.java
+++ login/src/main/java/login/controller/AuthController.java
@@ -1,23 +1,33 @@
 package login.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import login.model.dto.LoginRequest;
+import login.model.dto.RegisterRequest;
+import login.model.payload.AuthResponse;
+import login.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+	
+	private final AuthService authService;
 
 	@PostMapping("login")
-	public String login() {
-		return "login completado";
+	public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+		return ResponseEntity.ok(authService.login(loginRequest));
 	}
 	
+	//la base de datos descarta un usuario duplicado y pasa al siguiente ID (es normal?)
 	@PostMapping("register")
-	public String register() {
-		return "registro completado";
+	public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+		return ResponseEntity.ok(authService.register(registerRequest));
 	}
 }
